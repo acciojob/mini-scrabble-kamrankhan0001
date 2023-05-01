c84aeb3d51e25cbb4d0d60f0b293a9cefa8a34c6
MD KAMRAN KHAN <124767601+kamrankhan0001@users.noreply.github.com>
2023-05-01 13:05:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your code here
+let evalutedText=document.getElementById("evaluatedText");
+let letterCount=document.getElementById("letterCount");
+ evalutedText.addEventListener('input'),()=>{
+	let str = evalutedText.value;
+	letterCount.innerHtml=`${str.length}`;
+});
