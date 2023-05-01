7c65fd2399c5c2183f53b7aef946d11db9257e0d
MD KAMRAN KHAN <124767601+kamrankhan0001@users.noreply.github.com>
2023-05-01 13:22:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 //your code here
-let evaluatedText = document.getElementById("evaluatedText");
-let letterCount = document.getElementById("letterCount");
+// let evaluatedText = document.getElementById("evaluatedText");
+// let letterCount = document.getElementById("letterCount");
 
-evaluatedText.addEventListener('input', () => {
-	let str = evaluatedText.value;
+// evaluatedText.addEventListener('input', () => {
+// 	let str = evaluatedText.value;
+// 	letterCount.innerHTML = `${str.length}`;
+// });
+
+const evaluatedText = document.getElementById("evaluatedText");
+const letterCount = document.getElementById("letterCount");
+
+evaluatedText.addEventListener('input',()=>{
+	let str= evaluatedText.value;
 	letterCount.innerHTML = `${str.length}`;
 });
